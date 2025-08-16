e18dba8e5508007e404eb1dbb7fdc9fccfcaa012
Greem <kim0517@hotmail.com>
2025-08-17 02:45:20 +0900

    [Feat] 차트 채널 Response 데이터 수정 및 테이블 변경

--- supabase/functions/charts/EndPoint/ChartPlaylists.ts
+++ supabase/functions/charts/EndPoint/ChartPlaylists.ts
@@ -4,6 +4,17 @@ import { ErrorResponse, SuccessResponse } from "../../common/Models/Responses.ts
 import { FilterType } from "../Models/FilterType.ts";
 import { PeriodType } from "../Models/PeriodType.ts";
 import { MostPlaylistsQuery } from "../Queries/MostPlaylistsQuery.ts";
+import { YTPlaylistHead } from "../../common/Models/YTPlaylistHead.ts";
+
+type RequestPlaylistHeadResult = {
+  playlistHead: YTPlaylistHead | null;
+  failedID: string | null;
+}
+
+type PlaylistHeadsResponse = {
+  PlaylistHeads: YTPlaylistHead[];
+  FailedPlaylistIds: string[];
+}
 
 export class ChartsPlaylists {
     private supabase: SupabaseClient;
@@ -27,20 +38,27 @@ export class ChartsPlaylists {
         const limitCount: number = Number(params.get("limitcount")) ?? 10;
         switch(filterType){
           case FilterType.RECENT: {
-          const { data, error } = await this.supabase.from('RecentSqoopedPlaylists')
+          const { data, error } = await this.supabase.from('recent_sqooped_playlists')
           .select('id')
           .order('sqooped_date', { ascending: false })
           .limit(limitCount);
           if (error) return ErrorResponse(error.message, 500);
+
           const rows = Array.isArray(data) ? data : [];
-          return SuccessResponse(rows.map((x: { id: string }) => x.id));
+          const playlistIds = rows.map((x: { id: string }) => x.id);
+          const playlistHeads = await this.getPlaylistHeads(playlistIds);
+          return SuccessResponse(playlistHeads);
         }
         case FilterType.MOST: {
           const periodType: PeriodType | null = params.get("period") as PeriodType | null;
           if(periodType === null) return ErrorResponse(`잘못된 period 파라미터`, 400);
           const date: Date = new Date(params.get("date") as string);
           const {data, error} = await MostPlaylistsQuery.v1(this.supabase, periodType, date, limitCount);
-          return error ? ErrorResponse(error.message, 500) : SuccessResponse(data);
+          if(error) return ErrorResponse(error.message, 500);
+
+          const playlistIds: string[] = Array.isArray(data) ? data : [];
+          const playlistHeads = await this.getPlaylistHeads(playlistIds);
+          return SuccessResponse(playlistHeads);
         }
         default: {
           return ErrorResponse(`잘못된 period 파라미터 ${filterType}`, 400);
@@ -50,5 +68,33 @@ export class ChartsPlaylists {
         const message = err instanceof Error ? err.message : JSON.stringify(err);
         return ErrorResponse(message, 500);
       }
+    }
+
+    private async getPlaylistHeads(playlistIds: string[]): Promise<PlaylistHeadsResponse> {
+      const playlistResults: RequestPlaylistHeadResult[]  = await Promise.all(playlistIds.map(async (playlistId) => {
+        const { data: playlistHeadData, error: playlistHeadError } = await this.supabase.from('YTPlaylistHead').select('*').eq('id', playlistId).single();
+        if(playlistHeadError) {
+            return { playlistHead: null, failedID: playlistId };
+        }
+        const headTable = playlistHeadData as YTPlaylistHead;
+        const playlistHead: YTPlaylistHead = {
+            id: headTable.id,
+            originURL: headTable.originURL,
+            insertedDate: headTable.insertedDate,
+            isShazamed: headTable.isShazamed,
+            thumbnailURL: headTable.thumbnailURL,
+            title: headTable.title,
+            channel: headTable.channel,
+            ytPlaylistType: headTable.ytPlaylistType
+        }
+        return { playlistHead: playlistHead, failedID: null };
+    }));
+    const failedIds = playlistResults.filter(result => result.failedID !== null).map(result => result.failedID as string);
+    const playlistHeads = playlistResults.filter(result => result.playlistHead !== null).map(result => result.playlistHead as YTPlaylistHead);
+    const successResponse: PlaylistHeadsResponse = {
+      PlaylistHeads: playlistHeads ?? [],
+      FailedPlaylistIds: failedIds ?? []  
+    };
+    return successResponse;
     }
   }
